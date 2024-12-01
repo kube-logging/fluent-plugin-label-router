452200ca98740ba049cad638256e09df6f2f89d6
Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>
2024-12-01 23:02:33 +0100

    upgrade deps
    Signed-off-by: Peter Wilcsinszky <peter.wilcsinszky@axoflow.com>

--- lib/fluent/plugin/out_label_router.rb
+++ lib/fluent/plugin/out_label_router.rb
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
-require "fluent/plugin/output"
+require "fluent/plugin/bare_output"
 require 'prometheus/client'
 require 'concurrent'
 
